da5a62c24ff3b4cdd8c92f5c786364cd9943be90
raspitv <alex@raspi.tv>
2015-04-23 10:04:58 +0100

    Auto-install RasPiO Duino sketches and scripts
    Auto-install RasPiO Duino sketches and scripts

---
+++ duino.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# script to auto-install all the RasPiO Duino sketches on Pi
+
+# create sketchbook directory if dosn't exist
+mkdir /home/pi/sketchbook
+
+# give it the right permissions
+chmod 755 /home/pi/sketchbook
+
+# go to the /gome/pi directory
+cd /home/pi
+
+# clone the RasPiO Duino github repo
+git clone https://github.com/raspitv/raspio_duino.git
+
+# move the Python scripts to the /home/pi directory
+mv /home/pi/raspio_duino/*.py /home/pi
+
+# copy all the sketches to the sketchbook
+cp -r /home/pi/raspio_duino/* /home/pi/sketchbook
